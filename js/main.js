3c687f42841e3181c2e36ff72633a4e458801561
Kris Hedges <krishedges@gmail.com>
2018-02-17 02:48:20 -0500

    Add a nicer dev and build experience and include audio-presentation from npm

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-init = () => {
+let init = () => {
+
   let date = new Date();
   date.setMonth(date.getMonth() +6 );
   document.cookie = 'landing-page-visited=true; expires= ' + date.toGMTString();
+
+  let audioPresentation = new AudioPresentation();
+  audioPresentation.start();
 }
 
 document.onreadystatechange = () => {
